a8cdadb8f4cae9444db64520c4559f04d42c67b6
SantiagoFlorido <santiflorido.1016@gmail.com>
2025-03-29 19:41:10 -0500

    Selecciona tu rol bug fix

--- src/pages/Login/SelecionarRol.jsx
+++ src/pages/Login/SelecionarRol.jsx
@@ -8,7 +8,7 @@ const SelecionarRol = () => {
 
       <div>
         <div className="text-center bg-gray p-8 rounded-lg shadow-lg max-w-2xl w-full bg-gray-100">
-          <h1 className='text-4x1 text-[#009e4f] text-[30px] font-bold text-center mb-6'>Seleciona tu rol</h1>
+          <h1 className='text-4x1 text-[#009e4f] text-[30px] font-bold text-center mb-6'>Selecciona tu rol</h1>
             <button onClick={() => navigate('/Login/Teacher')} className='w-full mb-4 text-[18px] mt-6 rounded-full bg-emerald-700 text-white hover:bg-emerald-500 hover:text-white py-2 transition-colors duration-300 ' type='submit' >Docente</button>
             <button onClick={() => navigate('/Login/Student')} className='w-full mb-4 text-[18px] mt-6 rounded-full bg-emerald-700 text-white hover:bg-emerald-500 hover:text-white py-2 transition-colors duration-300 ' type='submit' >Estudiante</button>
         </div>
