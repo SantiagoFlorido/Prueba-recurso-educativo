bf0cb52cd13076f53b5b1ae3542e6307fefb57d1
SantiagoFlorido <santiflorido.1016@gmail.com>
2025-04-05 11:06:14 -0500

    ajustes de diseño menu principal

--- src/pages/pagina1/pagina1.jsx
+++ src/pages/pagina1/pagina1.jsx
@@ -70,12 +70,12 @@ const Pagina1 = () => {
           <img
             src="https://res.cloudinary.com/dufzsv87k/image/upload/v1741305038/logo-titulo_gtcapj.png"
             alt="Logo Universidad"
-            className="h-10 md:h-15"
+            className="h-9 md:h-15"
           />
           <img
             src="https://res.cloudinary.com/dufzsv87k/image/upload/v1743288905/logoeludec_qcilsr.png"
             alt="Logo Semillero"
-            className="h-9 md:h-14"
+            className="h-8 md:h-14"
           />
         </div>
 
--- src/pages/pagina3/pagina3.jsx
+++ src/pages/pagina3/pagina3.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaQuestionCircle } from 'react-icons/fa';
 
 const Pagina3 = () => {
   const navigate = useNavigate();
+  const [showHelp, setShowHelp] = useState(false);
 
   const handleLogout = () => {
     // Limpiar localStorage
@@ -12,38 +14,55 @@ const Pagina3 = () => {
     navigate('/');
   };
 
+  const toggleHelp = () => {
+    setShowHelp(!showHelp);
+  };
+
   return (
     <div className="bg-white p-4 w-full min-h-screen flex flex-col">
       {/* Fila superior: Menú Principal, Log in/Log up y Close */}
       <div className="flex justify-between items-center mb-4">
-        <h1 className="text-xl md:text-2xl font-bold">Menú Principal</h1>
-
-        <div className="flex items-center gap-2">
-          <div className="flex flex-col md:flex-row gap-2">
+        {/* Header con logos y botón */}
+        <div className="flex flex-row justify-between items-center mb-4">
+          <div className="flex items-center gap-4">
+            <img
+              src="https://res.cloudinary.com/dufzsv87k/image/upload/v1741305038/logo-titulo_gtcapj.png"
+              alt="Logo Universidad"
+              className="h-9 md:h-15"
+            />
+            <img
+              src="https://res.cloudinary.com/dufzsv87k/image/upload/v1743288905/logoeludec_qcilsr.png"
+              alt="Logo Semillero"
+              className="h-8 md:h-14"
+            />
+          </div>
+        </div>
+        <div className="flex md:flex-row items-center gap-1 flex-col">
+          <div className="flex flex-col md:flex-row gap-2 w-full">
             <button 
-              className="bg-green-600 text-white px-3 py-1 rounded-lg text-sm hover:bg-green-700 cursor-pointer" 
+              className="bg-green-600 text-white px-3 py-1 rounded-lg text-sm hover:bg-green-700 cursor-pointer w-full md:w-27" 
               onClick={handleLogout}
             >
               Cerrar Sesión
             </button>
           </div>
-          <button className="bg-gray-200 px-3 py-1 rounded-lg text-sm hover:bg-gray-300 cursor-pointer" onClick={() => navigate('/')}>Inicio</button>
+          <button className="bg-gray-200 px-3 py-1 rounded-lg text-sm hover:bg-gray-300 cursor-pointer w-full" onClick={() => navigate('/')}>
+            Inicio
+          </button>
+          <button 
+            className="bg-gray-100 px-3 py-2 rounded-lg text-sm hover:bg-gray-200 cursor-pointer w-full flex items-center justify-center"
+            onClick={toggleHelp}
+          >
+            <FaQuestionCircle className="text-green-600" />
+          </button>
         </div>
       </div>
 
       {/* Contenedor con 3 columnas y 3 filas */}
       <div className="grid grid-cols-1 md:grid-cols-[minmax(200px,_1fr)_minmax(100px,_0.3fr)_240px] gap-4 flex-grow">
         {/* Proyectos (Columna 1, Fila 1) */}
-        <div className="bg-green-100 p-2 rounded-md hover:bg-green-200 transition-colors flex flex-col min-h-[180px] cursor-pointer">
-          <h2 className="text-lg font-bold text-center" >Proyectos</h2>
-          <div className="grid grid-cols-2 gap-3 mt-2 flex-grow">
-            <div className="bg-blue-100 p-2 text-center rounded-md content-center flex items-center justify-center hover:bg-blue-200 transition-colors" onClick={() => navigate('/Conexión')}>
-              Formas de conexión
-            </div>
-            <div className="bg-red-100 p-2 rounded-md text-center content-center flex items-center justify-center hover:bg-red-200 transition-colors" onClick={() => navigate('/Proyectos')}>
-              Proyectos 0 de 6
-            </div>
-          </div>
+        <div className="flex items-center justify-center bg-gray-100 p-2 rounded-md hover:bg-gray-200 transition-colors  min-h-[180px] cursor-pointer text-lg font-bold text-center" onClick={() => navigate('/Proyectos')}>
+          {showHelp ? "Banner que indica la funcion del boton" : "Proyectos 0 de 6"}
         </div>
 
         {/* Recursos (Columna 2, Fila 1) */}
@@ -52,18 +71,25 @@ const Pagina3 = () => {
             className="bg-gray-100 p-2 rounded-lg flex-grow flex items-center justify-center hover:bg-gray-200 cursor-pointer"
             onClick={() => window.open('https://drive.google.com/drive/folders/1BRRCv8quGLVnSsB9ndCHO7cdWUf9sh3-?usp=sharing', '_blank', 'noopener,noreferrer')}   
           >
-            <h2 className="text-lg font-bold text-center">Recursos Gestor</h2>
+            <h2 className="text-lg text-center font-semibold">
+              {showHelp ? "Banner que indica la funcion del boton" : "Recursos Gestor"}
+            </h2>
           </div>
           <div 
             className="bg-gray-100 p-2 rounded-lg flex-grow flex items-center justify-center hover:bg-gray-200 cursor-pointer"
             onClick={() => window.open('https://drive.google.com/drive/folders/1_ziaHW8TNaqfAI8Tcc5e2UBrzhm8Gl6N?usp=sharing', '_blank', 'noopener,noreferrer')}
           >
-            <h2 className="text-lg font-bold text-center">Recursos de apoyo</h2>
+            <h2 className="text-lg text-center font-semibold">
+              {showHelp ? "Banner que indica la funcion del boton" : "Recursos de apoyo"}
+            </h2>
+          </div>
+          <div className="bg-gray-100 p-2 rounded-lg flex-grow flex items-center justify-center hover:bg-gray-200 cursor-pointer text-lg text-center font-semibold" onClick={() => navigate('/Conexión')}>
+            {showHelp ? "Banner que indica la funcion del boton" : "Formas de conexión"}
           </div>
         </div>
 
         {/* Proyectos Guardados (Columna 3, ocupa todas las filas) */}
-        <div className="row-span-3 bg-gray-100 p-2 rounded-lg">
+        <div className="row-span-2 bg-gray-100 p-2 rounded-lg">
           <h2 className="text-lg font-bold mb-2 text-center">Proyectos Guardados</h2>
           <div className="space-y-2">
             <div className="bg-white p-2 rounded-lg shadow-sm cursor-pointer">
@@ -84,22 +110,30 @@ const Pagina3 = () => {
             <div className="bg-white p-2 rounded-lg shadow-sm cursor-pointer">
               <h3 className="font-semibold">Proyecto Guardado 6</h3>
             </div>
+            <div className="bg-white p-2 rounded-lg shadow-sm cursor-pointer">
+              <h3 className="font-semibold">Proyecto Guardado 7</h3>
+            </div>
+            <div className="bg-white p-2 rounded-lg shadow-sm cursor-pointer">
+              <h3 className="font-semibold">Proyecto Guardado 8</h3>
+            </div>
           </div>
         </div>
 
-        {/* Imagen Temas del Proyecto (Columna 1-2, Fila 2) */}
-        <div className="col-span-1 md:col-span-2 bg-gray-100 p-2 rounded-lg md:h-full [&:has(+_div)]:mb-1">
-          <img
-            src="https://res.cloudinary.com/dufzsv87k/image/upload/v1743284192/Imagenes%20Recurso%20Educativo/Banner%20menu%20principal/vr8kxu1robbc9h0jxef6.jpg"
-            alt="Temas del proyecto"
-            className="w-full h-full object-fill rounded-md"
-          />
-        </div>
-
-        {/* Créditos (Columna 1-2, Fila 3) */}
-        <div className="col-span-1 md:col-span-2 bg-gray-100 p-2 rounded-lg">
-          <h2 className="text-lg font-bold text-center">Créditos</h2>
-          <p className="text-sm text-center">Aquí puedes agregar los créditos relacionados con los temas y proyectos.</p>
+        {/* Contenedor combinado para Imagen y Créditos */}
+        <div className="col-span-1 md:col-span-2">
+          {/* Imagen Temas del Proyecto */}
+          <div className="bg-gray-100 p-2 rounded-lg">
+            <img
+              src="https://res.cloudinary.com/dufzsv87k/image/upload/v1743284192/Imagenes%20Recurso%20Educativo/Banner%20menu%20principal/vr8kxu1robbc9h0jxef6.jpg"
+              alt="Banner "
+              className="w-full h-full object-fill rounded-md"
+            />
+          </div>
+          {/* Créditos */}
+          <div className="bg-gray-100 p-2 rounded-lg mt-4">
+            <h2 className="text-lg font-bold text-center">Créditos</h2>
+            <p className="text-sm text-center">Aquí puedes agregar los créditos relacionados con los temas y proyectos.</p>
+          </div>
         </div>
       </div>
     </div>
